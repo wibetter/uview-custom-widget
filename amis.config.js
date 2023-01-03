454ae2d060c61dc1b1fb3de3c971190f59553bc9
wibetter <365533093@qq.com>
2023-01-03 17:31:49 +0800

    feat: 支持本地Editor调试自定义组件

--- amis.config.js
+++ amis.config.js
@@ -21,6 +21,7 @@ module.exports = {
       alias: {
         '@': resolve('src'),
       },
+      conditionNames: ['require']
     },
     // createDeclaration: true, // 打包时是否创建ts声明文件
     ignoreNodeModules: false, // 打包时是否忽略 node_modules
@@ -31,6 +32,22 @@ module.exports = {
     cssLoaderUrl: true,
     moduleRules: [], // 用于配置自定义loaders
     plugins: [], // 用于配置自定义plugins // uni()
+  },
+  dev: {
+    entry: { // 本地调试模式的入口
+      index: ['./build/linkDebug.js'],
+    },
+    // 用于开启本地调试模式的相关配置信息
+    NODE_ENV: 'development',
+    port: 80,
+    autoOpenBrowser: true,
+    assetsPublicPath: '/', // 设置静态资源的引用路径（根域名+路径）
+    assetsSubDirectory: '',
+    hostname: 'localhost',
+    cssSourceMap: false,
+    closeHotReload: false, // 是否关闭热更新
+    closeEditorClient: false, // 是否关闭自动注入editor
+    editorClient: 'aipage'
   },
   linkDebug: {
     entry: { // 外链调试（爱速搭中预览本地自定义组件）
